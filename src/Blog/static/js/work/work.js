45c660a3b4393045e34d5f8f94609f24727babc9
hudsvi <hudsvi@139.com>
2018-10-29 11:32:39 +0800

    链接失效问题

--- src/Blog/static/js/work/work.js
+++ src/Blog/static/js/work/work.js
@@ -13,3 +13,14 @@ function appendUID(obj) {
     var href = "/blog/article/" + id + "?uuid=" + uuid + ":" + id;
     go(href);
 }
+
+//用于生成UID，
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c ==='x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
